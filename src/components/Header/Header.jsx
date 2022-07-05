dd3c171c121a1fae96bb7d41a220d4796dc9c376
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-05 15:25:39 +0500

    feat: add and styled button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,10 @@ function Header() {
 
   return (
     <header className="header">
+      <a className="header__logo" href='https://en.wikipedia.org/wiki/Tic-tac-toe' 
+      rel="noreferrer" target="_blank" title="Wiki">-</a>
       <h1 className="header__title">Tic-Tac-Toe</h1>
+      <button className="header__reset"></button>
     </header>
   );
 }
